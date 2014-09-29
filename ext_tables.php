e09d4c2027d653e960cb35e4613ccecd8433505b
Benjamin Franzke <benjaminfranzke@googlemail.com>
2014-09-29 09:51:42 +0200

    gallery. Use the tt_content image icon for the CType select box

--- ext_tables.php
+++ ext_tables.php
@@ -8,7 +8,7 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem('tt_content', 'CType', array(
 	'Gallerie',
 	'qbbootstrap_gallery',
-	'gfx/c_wiz/images_only.gif',
+	'i/tt_content_image.gif',
 ), 'textpic', 'after');
 
 $GLOBALS['TCA']['tt_content']['types']['qbbootstrap_gallery']['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.general;general, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.header;header,  image, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.imagelinks;imagelinks, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.visibility;visibility, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.access;access, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.extended, --div--;LLL:EXT:lang/locallang_tca.xlf:sys_category.tabs.category, categories, tx_gridelements_container, tx_gridelements_columns';
