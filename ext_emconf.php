ee736e61a94c1f4c5c48cc5b3e295c521d2bdc78
Benjamin Franzke <benjaminfranzke@googlemail.com>
2015-12-29 21:06:01 +0100

    Move to PSR-2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,36 +11,34 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Qbus Bootstrap',
-	'description' => '',
-	'category' => 'templates',
-	'author' => 'Benjamin Franzke',
-	'author_email' => 'bfr@qbus.de',
-	'author_company' => 'Qbus',
-	'shy' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '1.0.0',
-	'constraints' => array(
-		'depends' => array(
-			'extbase' => '6.0',
-			'fluid' => '6.0',
-			'typo3' => '6.0',
-			'gridelements' => '3.2.0',
-			'qbtools' => '1.4.0',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Qbus Bootstrap',
+    'description' => '',
+    'category' => 'templates',
+    'author' => 'Benjamin Franzke',
+    'author_email' => 'bfr@qbus.de',
+    'author_company' => 'Qbus',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'extbase' => '6.0',
+            'fluid' => '6.0',
+            'typo3' => '6.0',
+            'gridelements' => '3.2.0',
+            'qbtools' => '1.4.0',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
-
-?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:qbbootstrap/Configuration/TSConfig/Page.txt">');
-
-?>
