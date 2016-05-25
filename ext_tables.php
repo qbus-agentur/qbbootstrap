3b1223a5fddd6fb143add6e419e0a694439d745d
Benjamin Franzke <bfr@qbus.de>
2016-05-25 10:27:23 +0200

    qbootstrap_gallery: Add the grielements tab label as gridelements does now

--- ext_tables.php
+++ ext_tables.php
@@ -11,7 +11,7 @@ if (!defined('TYPO3_MODE')) {
     'i/tt_content_image.gif',
 ), 'textpic', 'after');
 
-$GLOBALS['TCA']['tt_content']['types']['qbbootstrap_gallery']['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.general;general, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.header;header,  image, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.imagelinks;imagelinks, layout, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.visibility;visibility, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.access;access, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.extended, --div--;LLL:EXT:lang/locallang_tca.xlf:sys_category.tabs.category, categories, tx_gridelements_container, tx_gridelements_columns';
+$GLOBALS['TCA']['tt_content']['types']['qbbootstrap_gallery']['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.general;general, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.header;header,  image, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.imagelinks;imagelinks, layout, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.visibility;visibility, --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.access;access, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.extended, --div--;LLL:EXT:lang/locallang_tca.xlf:sys_category.tabs.category, categories, --div--;LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:gridElements, tx_gridelements_container, tx_gridelements_columns';
 
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['qbbootstrap_gallery'] = 'mimetypes-x-content-image';
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicons']['qbbootstrap_gallery'] = 'tt_content_image.gif';
