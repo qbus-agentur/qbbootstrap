1de17cbba438182e951e42726eea2c24de17b975
Benjamin Franzke <bfr@qbus.de>
2022-08-29 09:27:00 +0200

    gallery: Fix wizard icon

---
+++ Configuration/Icons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+   // use same identifier as used in TSconfig for icon
+   'qbbootstrap-gallery' => [
+      'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+      'source' => 'EXT:qbbootstrap/Resources/Public/Icons/carousel.png',
+   ],
+];
